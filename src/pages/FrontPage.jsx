b52c5a2c30c2084b2e7e5033a125f87d39f4a1a1
andykr1k <69021102+andykr1k@users.noreply.github.com>
2022-08-20 11:12:59 -0700

    Setting Up Routing

--- src/pages/FrontPage.jsx
+++ src/pages/FrontPage.jsx
@@ -2,8 +2,10 @@
 
 export default function FrontPage() {
     return (
-      <div >
-        <a href="/hello">Hello</a>
+      <div className="m-5 space-x-2">
+        <a href="/">Home</a>
+        <a href="/about">About Me</a>
+        <a href="/projects">Projects</a>
       </div>
     )
 }
